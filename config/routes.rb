51eaeeb58ac329d7cbacb6eda62ee915ea78e72d
chriss26 <christian.calderono@utem.cl>
2017-06-13 21:34:46 -0400

    add uris y vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :proveedor_insumos
+  resources :mesas
+  resources :proveedors
+  get 'welcome/index'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
