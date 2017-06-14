f4998f7edb0ac858e983a1d418e3c71a2896835b
Gonzalo Rivas <Gonzalo.RivasV@utem.cl>
2017-06-14 00:30:17 -0400

    Agregadas las uris

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'empleados/', to: 'empleados#index'
-  get 'empleados/:id', to: 'empleados#mostrar'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :empleados
+  resources :insumo_platos
+  resources :pedidos
 end
